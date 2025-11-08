fa549a3df2ba2c0100fe5320f21376e9c2959c18
ILHAM AHSAN SAPUTRA <ilhamahsansaputra@gmail.com>
2025-11-08 14:04:47 +0700

    Setup Global Config

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import ContentSection from "@/app/components/ContentSection";
 export default function HomePage() {
   return (
     <main className="flex flex-col min-h-screen bg-[#f9f6f1] text-[#3b2f2f]">
-   
-
+  
       {/* Content Section */}
       <section id="about" className="py-16 px-6 sm:px-10 lg:px-20">
         <ContentSection />
